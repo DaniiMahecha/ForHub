5fb8d8cce2d9dede1c3587d40aa31ba7d9bfed32
DaniiMahecha <danielfelipemahechapena1010@gmail.com>
2026-03-22 20:14:12 -0500

    Represent Respuesta Data

---
+++ src/main/java/com/example/forohub/domain/respuesta/dto_respuesta/RespuestaData.java
@@ -0,0 +1,18 @@
+package com.example.forohub.domain.respuesta.dto_respuesta;
+
+import com.example.forohub.domain.topico.dto_topico.TopicoData;
+import com.example.forohub.domain.usuario.dto_usuario.UsuarioData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RespuestaData(
+        @NotBlank
+        String mensaje,
+        @NotBlank
+        String solucion,
+        @NotNull @Valid
+        TopicoData topico,
+        @NotNull @Valid
+        UsuarioData autor
+) {}
