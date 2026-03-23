292c7702b592f872ed6fc83921bc2a26574be874
DaniiMahecha <danielfelipemahechapena1010@gmail.com>
2026-03-22 21:46:03 -0500

    @EntityGraph

--- src/main/java/com/example/forohub/domain/topico/TopicoRepository.java
+++ src/main/java/com/example/forohub/domain/topico/TopicoRepository.java
@@ -7,7 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
-    /*Uso @EntityGraph cuando voy a acceder a relaciones LAZY en esa consulta específica y quiero evitar el N+1 query problem.*/
+    /*
+    Uso @EntityGraph cuando voy a acceder a relaciones LAZY en esa consulta específica y quiero evitar el N+1 query problem
+    y sé que existen relaciones entre entidades.
+    */
     @EntityGraph(attributePaths = {"autor", "curso"})
     Page<Topico> findAllByActivoTrue(Pageable pageable);
 
