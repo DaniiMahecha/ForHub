f2820acc77e0393c1949647c3f933ecc0d0a381f
DaniiMahecha <danielfelipemahechapena1010@gmail.com>
2026-04-04 14:00:53 -0500

    Remove the space for Topic in the DTO

--- src/main/java/com/example/forohub/domain/respuesta/dto_respuesta/RespuestaData.java
+++ src/main/java/com/example/forohub/domain/respuesta/dto_respuesta/RespuestaData.java
@@ -1,6 +1,5 @@
 package com.example.forohub.domain.respuesta.dto_respuesta;
 
-import com.example.forohub.domain.topico.dto_topico.TopicoData;
 import com.example.forohub.domain.usuario.dto_usuario.UsuarioData;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -9,8 +8,6 @@ import jakarta.validation.constraints.NotNull;
 public record RespuestaData(
         @NotBlank
         String mensaje,
-        @NotNull @Valid
-        TopicoData topico,
         @NotNull @Valid
         UsuarioData autor
 ) {}
